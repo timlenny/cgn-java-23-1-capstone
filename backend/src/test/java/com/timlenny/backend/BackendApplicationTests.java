3414bc1cae07be342d7ff93c51f0e6f6976b5775
timlenny <schlotter.tim@gmail.com>
2023-03-15 10:28:48 +0100

    Project setup

---
+++ backend/src/test/java/com/timlenny/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timlenny.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
