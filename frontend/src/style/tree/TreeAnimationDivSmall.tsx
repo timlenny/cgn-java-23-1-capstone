3beb1c166babcbf3e62371e7e2129f935b5f3e46
timlenny <schlotter.tim@gmail.com>
2023-04-06 15:11:57 +0200

    Optimized code tree animation

--- frontend/src/style/tree/TreeAnimationDivSmall.tsx
+++ frontend/src/style/tree/TreeAnimationDivSmall.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import './TreeAnimation.css';
 import {playAgain} from "./TreeAnimationFunction";
+import TreeBuildBasic from "./TreeBuildBasic";
 
 export default function TreeAnimationDivSmall() {
 
@@ -12,59 +13,7 @@ export default function TreeAnimationDivSmall() {
         <div>
             <svg id="animation" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                  viewBox="0 0 302.7 436.37">
-                <defs>
-                    <linearGradient id="Dégradé_sans_nom_18" data-name="Dégradé sans nom 18" x1="156.31"
-                                    y1="365.15" x2="246.31" y2="431.81" gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stopColor="#39505d"/>
-                        <stop offset="1" stopColor="#39505d" stopOpacity="0"/>
-                    </linearGradient>
-                    <linearGradient id="Dégradé_sans_nom_4" data-name="Dégradé sans nom 4" x1="200.34"
-                                    y1="253.69" x2="176.67" y2="221.09" gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stopColor="#929669"/>
-                        <stop offset="0.23" stopColor="#929669"/>
-                        <stop offset="1" stopColor="#fdfcf1"/>
-                    </linearGradient>
-                    <linearGradient id="Dégradé_sans_nom_2" data-name="Dégradé sans nom 2" x1="303.2"
-                                    y1="168.65" x2="213.62" y2="249.02" gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stopColor="#45aa3a"/>
-                        <stop offset="1" stopColor="#3d9439"/>
-                    </linearGradient>
-                    <linearGradient id="Dégradé_sans_nom_3" data-name="Dégradé sans nom 3" x1="236.83"
-                                    y1="175.48" x2="254.62" y2="212.29" gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stopColor="#41a83b"/>
-                        <stop offset="1" stopColor="#5ebc44"/>
-                    </linearGradient>
-                    <linearGradient id="Dégradé_sans_nom_4-2" x1="196.6" y1="195.74" x2="177" y2="178.38"
-                                    xlinkHref="#Dégradé_sans_nom_4"/>
-                    <linearGradient id="Dégradé_sans_nom_3-2" x1="296.11" y1="44.64" x2="176.48" y2="155.68"
-                                    xlinkHref="#Dégradé_sans_nom_3"/>
-                    <linearGradient id="Dégradé_sans_nom_2-2" x1="274.3" y1="150.3" x2="231.97" y2="99.99"
-                                    xlinkHref="#Dégradé_sans_nom_2"/>
-                    <linearGradient id="Dégradé_sans_nom_4-3" x1="-19.56" y1="223.16" x2="218.1" y2="279.92"
-                                    xlinkHref="#Dégradé_sans_nom_4"/>
-                    <linearGradient id="Dégradé_sans_nom_2-3" x1="16.11" y1="204.08" x2="92.18" y2="276.48"
-                                    xlinkHref="#Dégradé_sans_nom_2"/>
-                    <linearGradient id="Dégradé_sans_nom_2-4" x1="106.7" y1="255.03" x2="106.7" y2="255.03"
-                                    xlinkHref="#Dégradé_sans_nom_2"/>
-                    <linearGradient id="Dégradé_sans_nom_3-3" x1="63.36" y1="209.36" x2="54.16" y2="240.65"
-                                    xlinkHref="#Dégradé_sans_nom_3"/>
-                    <linearGradient id="Dégradé_sans_nom_4-4" x1="75.32" y1="182.31" x2="144.32" y2="160.33"
-                                    xlinkHref="#Dégradé_sans_nom_4"/>
-                    <linearGradient id="Dégradé_sans_nom_2-5" x1="19.89" y1="76.27" x2="141.37" y2="193.45"
-                                    xlinkHref="#Dégradé_sans_nom_2"/>
-                    <linearGradient id="Dégradé_sans_nom_3-4" x1="38.58" y1="178.83" x2="83.36" y2="134.66"
-                                    xlinkHref="#Dégradé_sans_nom_3"/>
-                    <linearGradient id="Dégradé_sans_nom_39" data-name="Dégradé sans nom 39" x1="148.36"
-                                    y1="248.02" x2="163.23" y2="248.02" gradientUnits="userSpaceOnUse">
-                        <stop offset="0" stopColor="#cbccb1"/>
-                        <stop offset="0.69" stopColor="#a8ab84"/>
-                        <stop offset="1" stopColor="#abae89"/>
-                    </linearGradient>
-                    <linearGradient id="Dégradé_sans_nom_2-6" x1="147.41" y1="4.1" x2="181.15" y2="164.83"
-                                    xlinkHref="#Dégradé_sans_nom_2"/>
-                    <linearGradient id="Dégradé_sans_nom_3-5" x1="98.37" y1="94.42" x2="168.31" y2="80.31"
-                                    xlinkHref="#Dégradé_sans_nom_3"/>
-                </defs>
+                {TreeBuildBasic()}
                 <title>mongo-tree</title>
                 <g id="shadow">
                     <path
